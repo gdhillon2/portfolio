ac3566b250bfa22aa8860112459bcb544cc35e33
gurvir dhillon <gman.dhillon@gmail.com>
2025-01-02 03:52:39 -0800

    made card component more flexible, added a spot for where i want my work title to go and also a resume download button

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -3,13 +3,15 @@ import React from "react";
 
 interface CardProps {
     content: string;
-    color: string;
+    bgColor?: string;
+    textStyles?: string;
+    size?: string;
 }
 
-export default function Card({content, color}: CardProps) {
+export default function Card({content, bgColor, textStyles, size}: CardProps) {
     return (
-        <div className={`flex flex-col justify-center items-center w-full h-full base-text ${color} rounded-lg`}>
-            <div>{content}</div>
+        <div className={`flex flex-col justify-center items-center ${size} base-text ${bgColor} rounded-lg`}>
+            <div className={`${textStyles}`}>{content}</div>
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,27 +6,38 @@ export default function Home() {
         <div className="grid grid-cols-3 w-[50%] h-[75%] gap-3">
             <div className="hero-section">
                 <div className="animate-slide-in-left w-full h-full">
-                    <Card content="Hero Section" color="bg-slate-700"/>
+                    <Card content="Hero Section" size="w-full h-full" bgColor="bg-slate-700"/>
                 </div>
             </div>
             <div className="work-section">
-                <div className="opacity-0 animate-slide-in-right w-full h-full">
-                    <Card content="Work Experience" color="bg-slate-500"/>
+                <div className="opacity-0 animate-slide-in-right w-full h-full flex flex-col gap-2">
+                    <Card content="Work Experience" size="w-full h-full" bgColor="bg-slate-500"/>
+                    <div className="flex w-full animate-slide-in-right-delay-200 opacity-0">
+                        <Card 
+                            content="Download Resume Button" 
+                            size="p-4 w-full" 
+                            bgColor="bg-orange-400 hover:bg-orange-500"
+                            textStyles="text-black text-lg"
+                        />
+                    </div>
                 </div>
             </div>
             <div className="tech-section">
-                <div className="opacity-0 animate-slide-in-left w-full h-full">
-                <Card content="Technologies" color="bg-slate-600"/>
+                <div className="opacity-0 animate-slide-in-left w-full h-full flex flex-col gap-2">
+                    <div className="flex w-full animate-slide-in-left-delay-200 opacity-0">
+                        <Card content="Software Engineer" size="p-4 w-full" bgColor="bg-yellow-400" textStyles="text-black text-lg font-bold"/>
+                    </div>
+                    <Card content="Technologies" size="w-full h-full" bgColor="bg-slate-600"/>
                 </div>
             </div>
             <div>
                 <div className="w-full h-full">
-                    <Card content="Gurvir Dhillon" color="bg-slate-900"/>
+                    <Card content="Gurvir Dhillon" size="w-full h-full" bgColor="bg-slate-900"/>
                 </div>
             </div>
             <div className="education-section">
                 <div className="animate-slide-in-right w-full h-full">
-                    <Card content="Education" color="bg-slate-800"/>
+                    <Card content="Education" size="w-full h-full" bgColor="bg-slate-800"/>
                 </div>
             </div>
         </div>
