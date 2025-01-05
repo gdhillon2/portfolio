5af18756ad62769ec65189f92f63495793227172
gdhillon2 <gman.dhillon@gmail.com>
2025-01-05 07:21:09 -0800

    project card looks a lot cleaner now

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -3,12 +3,12 @@ interface ProjectCardProps {
     textContent?: string;
 }
 
-export default function ProjectCard({ projectTitle, textContent }: ProjectCardProps) {
+export default function ProjectCard({ projectTitle }: ProjectCardProps) {
     return (
         <div className="relative p-2 border border-gray-900 rounded-3xl flex w-full h-[500px] bg-gray-950">
             <div className="w-full glare-item-outer"></div>
             <div className="w-full border border-gray-700 rounded-2xl bg-slate-900 p-5 relative">
-            <div className="w-full glare-item-inner"></div>
+                <div className="w-full glare-item-inner"></div>
                 {projectTitle}
             </div>
         </div>
