f322471b8fa611246fa77e5ae6f8efd774c17224
gurvir dhillon <gman.dhillon@gmail.com>
2025-01-07 17:43:41 -0800

    added light effect to home page sections (still need to implement vertical glare)

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,9 @@ export default function Home() {
             <div className="grid grid-cols-3 w-[50%] h-[75%] gap-3 p-5">
                 <div className="hero-section">
                     <div className="animate-slide-in-left w-full h-full">
-                        <div className="relative w-full h-full p-1 rounded-lg border border-gray-700 base-text shadow-md">
-                            <div className="relative w-full h-full px-4 py-3 rounded-sm border border-gray-700 base-text shadow-md">
+                        <div className="relative w-full h-full p-1 rounded-lg border border-gray-900 base-text shadow-md bg-gray-950">
+                            <div className="relative w-full h-full px-4 py-3 rounded-sm border border-gray-700 base-text shadow-md bg-slate-900">
+                                <div className="corner-glare-item-inner-horizontal"></div>
                                 <div className="text-shadow text-xl font-bold">About Me</div>
                             </div>
                         </div>
@@ -25,8 +26,9 @@ export default function Home() {
                 </div>
                 <div className="work-section">
                     <div className="w-full h-full flex flex-col gap-2">
-                        <div className="w-full h-full p-1 rounded-lg border border-gray-700 base-text animate-slide-in-down opacity-0 shadow-md">
-                            <div className="relative w-full h-full px-4 py-3 rounded-sm border border-gray-700 base-text shadow-md">
+                        <div className="w-full h-full p-1 rounded-lg border border-gray-900 base-text animate-slide-in-down opacity-0 shadow-md bg-gray-950">
+                            <div className="relative w-full h-full px-4 py-3 rounded-sm border border-gray-700 base-text shadow-md bg-slate-900">
+                                <div className="corner-glare-item-inner-horizontal"></div>
                                 <div className="text-shadow text-xl font-bold">Work Experience</div>
                                 <div className="w-full h-full"></div>
                             </div>
@@ -44,15 +46,19 @@ export default function Home() {
                 <div className="tech-section">
                     <div className="w-full h-full flex flex-col gap-2">
                         <div className="flex w-full animate-slide-in-left-delay-200 opacity-0">
-                            <div className="text-shadow flex w-full h-full p-1 rounded-lg base-text text-lg font-bold border border-gray-700">
-                                <div className="relative w-full h-full px-4 py-3 rounded-sm border border-gray-700 base-text shadow-md">
+                            <div className="text-shadow flex w-full h-full p-1 rounded-lg base-text text-lg font-bold border border-gray-900 bg-gray-950">
+                                <div className="relative w-full h-full px-4 py-3 rounded-sm border border-gray-700 base-text shadow-md bg-slate-900">
+                                <div className="corner-glare-item-inner-horizontal"></div>
                                     Software Engineer
                                 </div>
                             </div>
                         </div>
-                        <div className="w-full h-full p-1 animate-slide-in-up rounded-lg border border-gray-700 base-text text-pink-400 shadow-md">
-                            <div className="relative w-full h-full px-4 py-3 rounded-sm border border-gray-700 base-text shadow-md">
-                                <div className="text-shadow text-xl font-bold">Technologies</div>
+                        <div className="w-full h-full p-1 animate-slide-in-up rounded-lg border border-gray-900 base-text text-pink-400 shadow-md bg-gray-950">
+                            <div className="relative w-full h-full px-4 py-3 rounded-sm border border-gray-700 base-text shadow-md bg-slate-900">
+                                <div className="corner-glare-item-inner-horizontal"></div>
+                                <div className="text-shadow text-xl font-bold">
+                                    Technologies
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -64,8 +70,9 @@ export default function Home() {
                 </div>
                 <div className="hobbies-section">
                     <div className="animate-slide-in-right w-full h-full">
-                        <div className="w-full h-full p-1 rounded-lg border border-gray-700 base-text shadow-md">
-                            <div className="relative w-full h-full px-4 py-3 rounded-sm border border-gray-700 base-text shadow-md">
+                        <div className="w-full h-full p-1 rounded-lg border border-gray-900 base-text shadow-md bg-gray-950">
+                            <div className="relative w-full h-full px-4 py-3 rounded-sm border border-gray-700 base-text shadow-md bg-slate-900">
+                                <div className="corner-glare-item-inner-horizontal"></div>
                                 <div className="text-shadow text-xl font-bold">Hobbies</div>
                             </div>
                         </div>
