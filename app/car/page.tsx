3b9c480c48ae0ee97e3c4f03fe366c56275bfe51
gurvir dhillon <gman.dhillon@gmail.com>
2025-01-27 22:41:05 -0800

    changed car page to say more info coming soon so people don't get confused

--- app/car/page.tsx
+++ app/car/page.tsx
@@ -1,5 +1,5 @@
 export default function SelfDrivingCar() {
     return (
-        <div className="text-white">Test</div>
+        <div className="flex w-full justify-center text-white">More info coming soon!</div>
     )
 }
