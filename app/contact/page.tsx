95cd4d55bc160c1401dc75567e558df1aed6d6fb
gdhillon2 <gman.dhillon@gmail.com>
2025-02-09 23:20:14 -0800

    added very basic cards for contacting me through either email/linkedin and also included github

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,13 @@
+import Card from "@/components/Card";
+
 export default function ContactPage() {
     return (
-        <div className="flex w-full justify-center">Contact Page Coming Soon</div>
+        <div className="flex flex-col items-center">
+            <div className="flex flex-col max-w-[600px] gap-5">
+                <Card content="Email: gurvirdhillonswe@gmail.com"/>
+                <Card content="GitHub: gdhillon2"/>
+                <Card content="LinkedIn: www.linkedin.com/in/gurvir-dhillon-802504295"/>
+            </div>
+        </div>
     )
 }
