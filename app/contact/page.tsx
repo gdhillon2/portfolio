183b74472f4e7fed618dcb34705d718ed787c438
gdhillon2 <gman.dhillon@gmail.com>
2025-03-02 18:03:04 -0800

    added contact card to contact page for testing

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,5 @@
 import Card from "@/components/Card";
+import ContactCard from "@/components/ContactCard";
 
 export default function ContactPage() {
     return (
@@ -7,6 +8,7 @@ export default function ContactPage() {
                 <Card content="Email: gurvirdhillonswe@gmail.com"/>
                 <Card content="GitHub: gdhillon2"/>
                 <Card content="LinkedIn: www.linkedin.com/in/gurvir-dhillon-802504295"/>
+                <ContactCard text="test" icon="test" copy_string="test" link=""/>
             </div>
         </div>
     )
